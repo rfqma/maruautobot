70b1ed0dbf2c052e94de1859f56913026da2a5a3
rfqma <abelcaster025@gmail.com>
2025-04-05 18:28:45 +0700

    update

--- index.ts
+++ index.ts
@@ -51,11 +51,11 @@ bot.command("tiktok", (ctx) => {
 });
 
 bot.command("instagram", (ctx) => {
-  // ctx.session.waitingForTikTokUrl = false;
-  // ctx.session.waitingForInstagramUrl = true;
-  // ctx.session.emptySession = false;
-  // ctx.reply("masukkan url/link instagram:");
-  ctx.reply("🚧 /instagram sedang dalam perbaikan");
+  ctx.session.waitingForTikTokUrl = false;
+  ctx.session.waitingForInstagramUrl = true;
+  ctx.session.emptySession = false;
+  ctx.reply("masukkan url/link instagram:");
+  // ctx.reply("🚧 /instagram sedang dalam perbaikan");
 });
 
 bot.command("reset", (ctx) => {
@@ -112,58 +112,103 @@ bot.on("message:text", async (ctx) => {
 
       try {
         const urlInfo = await getInstagramPost(url);
-        console.log(urlInfo);
-        // if (
-        //   urlInfo.video &&
-        //   (urlInfo.image === undefined || urlInfo.image.length === 0)
-        // ) {
-        //   const igVideo = urlInfo.video;
 
-        //   const videoGroup = igVideo.map((video: any) => {
-        //     return InputMediaBuilder.video(video.video);
-        //   });
+        const images =
+          urlInfo.media_details.filter((item) => item.type === "image") || [];
 
-        //   for (const i of videoGroup) {
-        //     await ctx.replyWithVideo(i.media);
-        //   }
+        const videos =
+          urlInfo.media_details.filter((item) => item.type === "video") || [];
 
-        //   ctx.reply(`✅ Berhasil unduh!`);
-        // } else if (
-        //   urlInfo.image &&
-        //   (urlInfo.video === undefined || urlInfo.video.length === 0)
-        // ) {
-        //   const igImage = urlInfo.image;
+        await ctx.reply(
+          `owner_username: ${urlInfo.post_info.owner_username}\nowner_fullname: ${urlInfo.post_info.owner_fullname}\nis_verified: ${urlInfo.post_info.is_verified}\nis_private: ${urlInfo.post_info.is_private}\nlikes: ${urlInfo.post_info.likes}\nis_ad: ${urlInfo.post_info.is_ad}\ncaption: ${urlInfo.post_info.caption}\n\nimages: ${images.length}\nvideos: ${videos.length}`
+        );
 
-        //   const imageGroup = igImage.map((image: any) => {
-        //     return InputMediaBuilder.photo(image);
-        //   });
+        if (videos.length > 0 && images.length === 0) {
+          const videoGroup = videos.map((item, index) => {
+            return InputMediaBuilder.video(item.url, {
+              caption: ` ${index + 1}/${videos.length}\n type: ${
+                item.type
+              }\n h: ${item.dimensions.height}\n w: ${
+                item.dimensions.width
+              }\n ${
+                item.video_view_count
+                  ? `video_view_count: ${item.video_view_count}`
+                  : ""
+              }`,
+            });
+          });
 
-        //   for (const i of imageGroup) {
-        //     await ctx.replyWithPhoto(i.media);
-        //   }
+          for (const i of videoGroup) {
+            await ctx.replyWithVideo(i.media);
+            await ctx.reply(i.caption ?? "");
+          }
 
-        //   ctx.reply(`✅ Berhasil unduh!`);
-        // } else {
-        //   const igVideo = urlInfo.video;
-        //   const igImage = urlInfo.image;
+          ctx.reply(`✅ Berhasil unduh!`);
+        } else if (images.length > 0 && videos.length === 0) {
+          const imageGroup = images.map((item, index) => {
+            return InputMediaBuilder.photo(item.url, {
+              caption: ` ${index + 1}/${images.length}\n type: ${
+                item.type
+              }\n h: ${item.dimensions.height}\n w: ${
+                item.dimensions.width
+              }\n ${
+                item.video_view_count
+                  ? `video_view_count: ${item.video_view_count}`
+                  : ""
+              }`,
+            });
+          });
 
-        //   const videoGroup = igVideo.map((video: any) => {
-        //     return InputMediaBuilder.video(video.video);
-        //   });
+          for (const i of imageGroup) {
+            await ctx.replyWithPhoto(i.media);
+            await ctx.reply(i.caption ?? "");
+          }
 
-        //   const imageGroup = igImage.map((image: any) => {
-        //     return InputMediaBuilder.photo(image);
-        //   });
+          ctx.reply(`✅ Berhasil unduh!`);
+        } else if (images.length > 0 && videos.length > 0) {
+          const imageGroup = images.map((item, index) => {
+            return InputMediaBuilder.photo(item.url, {
+              caption: ` ${index + 1}/${images.length}\n type: ${
+                item.type
+              }\n h: ${item.dimensions.height}\n w: ${
+                item.dimensions.width
+              }\n ${
+                item.video_view_count
+                  ? `video_view_count: ${item.video_view_count}`
+                  : ""
+              }`,
+            });
+          });
 
-        //   for (const i of imageGroup) {
-        //     await ctx.replyWithPhoto(i.media);
-        //   }
-        //   for (const i of videoGroup) {
-        //     await ctx.replyWithVideo(i.media);
-        //   }
-        //   ctx.reply(`✅ Berhasil unduh!`);
-        // }
-      } catch (error: any) {
+          const videoGroup = videos.map((item, index) => {
+            return InputMediaBuilder.video(item.url, {
+              caption: ` ${index + 1}/${videos.length}\n type: ${
+                item.type
+              }\n h: ${item.dimensions.height}\n w: ${
+                item.dimensions.width
+              }\n ${
+                item.video_view_count
+                  ? `video_view_count: ${item.video_view_count}`
+                  : ""
+              }`,
+            });
+          });
+
+          for (const i of imageGroup) {
+            await ctx.replyWithPhoto(i.media);
+            await ctx.reply(i.caption ?? "");
+          }
+
+          for (const i of videoGroup) {
+            await ctx.replyWithVideo(i.media);
+            await ctx.reply(i.caption ?? "");
+          }
+
+          ctx.reply(`✅ Berhasil unduh!`);
+        } else {
+          ctx.reply("⚠️ Tidak ditemukan media pada unggahan.");
+        }
+      } catch (error) {
         ctx.reply(`Gagal unduh, coba lagi!`);
       } finally {
         ctx.session.emptySession = true;
